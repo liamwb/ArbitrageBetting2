de800a26b29da630cabb63d2626c16051b21afec
Liam's Laptop <liam.woodbaker@gmail.com>
2020-04-10 13:12:55 +1000

    We can now get all the sports for which there are live odds

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ websites for odds, I will be using the-odds-api, which will get lots of odds fro
 import json
 import requests
 
+
+
+
 # Keep the API key secret
 api_key = open('api_key.txt').read()
 
-
 # First get a list of in-season sports
-sports_response = requests.get('https://api.the-odds-api.com/v3/sports', params={
-    'api_key': api_key
-})
+sports_response = requests.get('https://api.the-odds-api.com/v3/sports', params={'api_key': api_key})
+sports_json = json.loads(sports_response.text)
+sports_keys = [dictionary['key'] for dictionary in sports_json['data']]  # all the currently available sports
+
+# Now get the odds for each event in each sport for each agency
+
+
+
